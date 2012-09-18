71762b00e1fff83d2e680627f4612eed1726948b
Jack "Mrfence97" Brittan <ja.brittan@hotmail.co.uk>
2012-09-18 21:18:29 +0100

    Created First Files

---
+++ 3DEngine/3DEngine/EngineMain.cpp
@@ -0,0 +1,6 @@
+#include "EngineMain.h"
+
+int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPreviousInstance,LPSTR lpcmdline,int nCmdShow)
+{
+	return 0;
+}
---
+++ 3DEngine/3DEngine/stdh.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <d3dx9.h>
+#include <d3d9.h>
+#include <Windows.h>
+#include <string.h>
